f22668ceafa24103a1ad20ac438ee66307ee92e0
ツ Liverbool <nukboon@gmail.com>
2015-04-23 08:26:18 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,23 +4,21 @@
 for env in $(printenv); do
     IFS== read KEY VALUE <<< "$env"
     sed "s,\${${KEY}},${VALUE},g" -i /etc/nginx/sites-enabled/default
-    #echo "${KEY} => ${VALUE}";
 done;
 
 # replace value with variable
 for env in $(printenv); do
     IFS== read KEY VALUE <<< "$env"
     sed "s,\${${KEY}},${VALUE},g" -i /etc/nginx/sites-enabled/default
-    #echo "${KEY} => ${VALUE}";
 done;
 
 # http://symfony.com/doc/current/cookbook/configuration/external_parameters.html
-for env in $(printenv); do
-    IFS== read KEY VALUE <<< "$env"
-    if [[ "${KEY}" == SYMFONY__* ]]; then
-        export ${KEY}=$(eval echo '$'${VALUE})
-    fi
-done;
+#for env in $(printenv); do
+#    IFS== read KEY VALUE <<< "$env"
+#    if [[ "${KEY}" == SYMFONY__* ]]; then
+#        export ${KEY}=$(eval echo '$'${VALUE})
+#    fi
+#done;
 
 #exec cat /etc/nginx/sites-enabled/default
 exec nginx;
